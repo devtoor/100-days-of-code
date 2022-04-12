57c13fa49c6c52db5a693559f83cc461246d7701
Jay Morales <jaymorales@pm.me>
2022-04-12 19:11:28 -0400

    Initial pong-game

---
+++ pong-game/main.py
@@ -0,0 +1,52 @@
+from turtle import Screen
+from paddle import Paddle
+from ball import Ball
+from scoreboard import Scoreboard
+import time
+
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=800, height=600)
+screen.title("Pong")
+screen.tracer(0)
+
+l_paddle = Paddle((-350, 0))
+r_paddle = Paddle((350, 0))
+ball = Ball()
+scoreboard = Scoreboard()
+
+game_is_on = True
+
+
+def exit_game():
+    global game_is_on
+    game_is_on = False
+
+
+screen.listen()
+screen.onkey(l_paddle.move_up, "w")
+screen.onkey(l_paddle.move_down, "s")
+screen.onkey(r_paddle.move_up, "Up")
+screen.onkey(r_paddle.move_down, "Down")
+screen.onkey(exit_game, "x")
+
+while game_is_on:
+    time.sleep(ball.move_speed)
+    ball.move()
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.y_bounce()
+
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 325 or ball.distance(l_paddle) < 50 and ball.xcor() < -325:
+        ball.x_bounce()
+
+    if ball.xcor() > 370:
+        ball.reset_position()
+        scoreboard.increase_l_score()
+
+    if ball.xcor() < -370:
+        ball.reset_position()
+        scoreboard.increase_r_score()
+
+    screen.update()
+
+screen.exitonclick()
---
+++ pong-game/paddle.py
@@ -0,0 +1,21 @@
+from turtle import Turtle
+
+
+class Paddle(Turtle):
+
+    def __init__(self, position):
+        super().__init__()
+        self.shape("square")
+        self.color("white")
+        self.setheading(90)
+        self.turtlesize(stretch_wid=1, stretch_len=5)
+        self.penup()
+        self.goto(position[0], position[1])
+
+    def move_up(self):
+        if self.ycor() < 240:
+            self.forward(20)
+
+    def move_down(self):
+        if self.ycor() > -240:
+            self.backward(20)
