c4d350c6b8ac8d2e7356916f1ec0ed8c505b67fb
Jay Morales <jaymorales@pm.me>
2022-04-13 13:36:29 -0400

    Modify variable name to avoid confusion

--- mail-merge/main.py
+++ mail-merge/main.py
@@ -1,8 +1,8 @@
 with open("Input/Letters/starting_letter.txt") as draft:
     draft_letter = draft.read()
 
-with open("Input/Names/invited_names.txt") as invited_names:
-    invited_names = invited_names.read().split("\n")
+with open("Input/Names/invited_names.txt") as names_file:
+    invited_names = names_file.read().split("\n")
 
 for name in invited_names:
     with open(f"Output/ReadyToSend/letter_for_{name.title()}.txt", mode="w") as output:
