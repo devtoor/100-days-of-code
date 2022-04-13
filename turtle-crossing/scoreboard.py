1e5ea47baeba02042710adfa7e95175234eae15c
Jay Morales <jaymorales@pm.me>
2022-04-12 20:58:47 -0400

    Initial solution

--- turtle-crossing/scoreboard.py
+++ turtle-crossing/scoreboard.py
@@ -1,5 +1,26 @@
+from turtle import Turtle
+
 FONT = ("Courier", 24, "normal")
 
 
-class Scoreboard:
-    pass
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.hideturtle()
+        self.penup()
+        self.level = 1
+        self.goto(-285, 265)
+        self.show_score()
+
+    def show_score(self):
+        self.write(f"Level: {self.level}", align="left", font=FONT)
+
+    def level_up(self):
+        self.level += 1
+        self.clear()
+        self.show_score()
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("GAME OVER", align="center", font=FONT)
