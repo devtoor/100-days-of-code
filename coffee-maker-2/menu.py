2ec9e8e71728e815216b60cba9f6c42dacecb20f
Jay Morales <jaymorales@pm.me>
2022-04-19 17:46:45 -0400

    Clean up comment lines

--- coffee-maker-2/menu.py
+++ coffee-maker-2/menu.py
@@ -1,5 +1,5 @@
 class MenuItem:
-    """Models each Menu Item."""
+
     def __init__(self, name, water, milk, coffee, cost):
         self.name = name
         self.cost = cost
@@ -11,7 +11,7 @@ class MenuItem:
 
 
 class Menu:
-    """Models the Menu with drinks."""
+
     def __init__(self):
         self.menu = [
             MenuItem(name="latte", water=200, milk=150, coffee=24, cost=2.5),
@@ -20,14 +20,12 @@ class Menu:
         ]
 
     def get_items(self):
-        """Returns all the names of the available menu items"""
         options = ""
         for item in self.menu:
             options += f"{item.name}/"
         return options
 
     def find_drink(self, order_name):
-        """Searches the menu for a particular drink by name. Returns that item if it exists, otherwise returns None"""
         for item in self.menu:
             if item.name == order_name:
                 return item
--- coffee-maker-2/money_machine.py
+++ coffee-maker-2/money_machine.py
@@ -1,7 +1,6 @@
 class MoneyMachine:
 
     CURRENCY = "$"
-
     COIN_VALUES = {
         "quarters": 0.25,
         "dimes": 0.10,
@@ -14,18 +13,15 @@ class MoneyMachine:
         self.money_received = 0
 
     def report(self):
-        """Prints the current profit"""
         print(f"Money: {self.CURRENCY}{self.profit}")
 
     def process_coins(self):
-        """Returns the total calculated from coins inserted."""
         print("Please insert coins.")
         for coin in self.COIN_VALUES:
             self.money_received += int(input(f"How many {coin}?: ")) * self.COIN_VALUES[coin]
         return self.money_received
 
     def make_payment(self, cost):
-        """Returns True when payment is accepted, or False if insufficient."""
         self.process_coins()
         if self.money_received >= cost:
             change = round(self.money_received - cost, 2)
