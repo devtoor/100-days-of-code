d4996d124dc8194fb837e987e1c70e8a5586a3ee
Jay Morales <jaymorales@pm.me>
2022-04-21 15:54:40 -0400

    Initial top100-movies solution

---
+++ top100-movies/main.py
@@ -0,0 +1,13 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
+
+content = requests.get(URL).text
+soup = BeautifulSoup(content, "html.parser")
+
+titles = [item.getText() for item in soup.find_all(name="h3", class_="title")][::-1]
+
+with open("movies.txt", "w") as file:
+    for title in titles:
+        file.write(f"{title}\n")
