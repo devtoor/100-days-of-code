bfb7fd99909bf254f8bf4850fca81e5ef949fab6
Jay Morales <jaymorales@pm.me>
2022-04-21 19:30:51 -0400

    Initial amazon-price-tracker solution

---
+++ amazon-price-tracker/main.py
@@ -0,0 +1,32 @@
+import os
+import smtplib
+
+import requests
+from bs4 import BeautifulSoup
+from dotenv import load_dotenv
+
+BUY_PRICE = 120
+URL = "https://www.amazon.com/dp/B07662ZD8J/ref=twister_B09LN4Q7H4?_encoding=UTF8&th=1"
+HEADER = {
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 "
+                  "(KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36",
+    "Accept-Language": "en-US,en;q=0.9"
+}
+
+response = requests.get(URL, headers=HEADER)
+soup = BeautifulSoup(response.content, "lxml")
+
+price = float(soup.find(name="span", class_="a-offscreen").get_text().split("$")[1])
+title = soup.find(name="span", id="productTitle").get_text().strip()
+
+load_dotenv()
+if price < BUY_PRICE:
+    message = f"{title} is now ${price}"
+    with smtplib.SMTP(os.environ.get("SMTP_ADDRESS"), port=587) as connection:
+        connection.starttls()
+        connection.login(os.environ.get("EMAIL"), os.environ.get("PASSWORD"))
+        connection.sendmail(
+            from_addr=os.environ.get("EMAIL"),
+            to_addrs=os.environ.get("EMAIL"),
+            msg=f"Subject:Amazon Price Alert!\n\n{message}\n{URL}"
+        )
