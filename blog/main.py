7e0fba95831d5dbf9358d0d109d0fdc2054121be
Jay Morales <jaymorales@pm.me>
2022-04-25 10:30:29 -0400

    Initial blog solution

---
+++ blog/main.py
@@ -0,0 +1,27 @@
+import requests as requests
+from flask import Flask, render_template
+
+from post import Post
+
+app = Flask(__name__)
+
+posts = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
+post_objects = [Post(post["id"], post["title"], post["subtitle"], post["body"]) for post in posts]
+
+
+@app.route('/')
+def home():
+    return render_template("index.html", posts=post_objects)
+
+
+@app.route('/post/<int:index>')
+def show_post(index):
+    requested_post = None
+    for blog_post in post_objects:
+        if blog_post.id == index:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
