0e0e6878f3f05ed3c17aafaaf1c877b3fa0f6347
Jay Morales <jaymorales@pm.me>
2022-04-28 15:50:41 -0400

    Add form function

--- bootstrap-blog/main.py
+++ bootstrap-blog/main.py
@@ -1,16 +1,21 @@
-from flask import Flask, render_template
+import smtplib
+from os import environ as env
+
 import requests
+from dotenv import load_dotenv
+from flask import Flask, render_template, request
 
 posts = requests.get(
     "https://gist.githubusercontent.com/gellowg/389b1e4d6ff8effac71badff67e4d388/raw"
     "/fc31e41f8e1a6b713eafb9859f3f7e335939d518/data.json"
 ).json()
 
+load_dotenv()
 app = Flask(__name__)
 
 
 @app.route("/")
-def get_all_posts():
+def home():
     return render_template("index.html", all_posts=posts)
 
 
@@ -19,9 +24,13 @@ def about():
     return render_template("about.html")
 
 
-@app.route("/contact")
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
-    return render_template("contact.html")
+    if request.method == "POST":
+        form = request.form
+        send_email(form["name"], form["email"], form["phone"], form["message"])
+        return render_template("contact.html", msg_sent=True)
+    return render_template("contact.html", msg_sent=False)
 
 
 @app.route("/post/<int:index>")
@@ -31,6 +40,14 @@ def show_post(index):
         if blog_post["id"] == index:
             requested_post = blog_post
     return render_template("post.html", post=requested_post)
+
+
+def send_email(name, email, phone, message):
+    email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
+    with smtplib.SMTP(env.get("EMAIL_SMTP"), port=587) as connection:
+        connection.starttls()
+        connection.login(env.get("EMAIL"), env.get("EMAIL_PASSWORD"))
+        connection.sendmail(from_addr=env.get("EMAIL"), to_addrs=env.get("EMAIL"), msg=email_message)
 
 
 if __name__ == "__main__":
