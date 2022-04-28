bd196123d464a60b6e1be03d6a80c6a1560d5433
Jay Morales <jaymorales@pm.me>
2022-04-28 11:30:17 -0400

    Initial html-forms solution

---
+++ html-forms/main.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    return f"<h1>Name: {request.form['username']}, Password: {request.form['password']}</h1>"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
