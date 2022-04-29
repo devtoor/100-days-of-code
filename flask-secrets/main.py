efc142706936a172f4c4128e45da6f8a2ff62cf5
Jay Morales <jaymorales@pm.me>
2022-04-28 20:27:40 -0400

    Initial flask-secret solution

--- flask-secrets/main.py
+++ flask-secrets/main.py
@@ -1,11 +1,34 @@
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import Email, Length, InputRequired
 
 app = Flask(__name__)
+app.secret_key = "dnlkv4cxnvf123ksljnmnio1wokjon"
+Bootstrap(app)
+
+
+class LoginForm(FlaskForm):
+    email = StringField(label="Email", validators=[InputRequired(), Email()])
+    password = PasswordField(label="Password", validators=[InputRequired(), Length(min=8)])
+    submit = SubmitField(label="Log In")
 
 
 @app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == "admin@email.com" and form.password.data == "12345678":
+            return render_template("success.html")
+        else:
+            return render_template("denied.html")
+    return render_template("login.html", form=form)
 
 
 if __name__ == '__main__':
