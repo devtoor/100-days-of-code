ba3f3ac96d18f3c6ff3483cdb58ab00ac5b658cf
Jay Morales <jaymorales@pm.me>
2022-04-30 11:04:35 -0400

    Run pre-commit hook

--- amazon-price-tracker/main.py
+++ amazon-price-tracker/main.py
@@ -9,8 +9,8 @@ BUY_PRICE = 120
 URL = "https://www.amazon.com/dp/B07662ZD8J/ref=twister_B09LN4Q7H4?_encoding=UTF8&th=1"
 HEADER = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 "
-                  "(KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36",
-    "Accept-Language": "en-US,en;q=0.9"
+    "(KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36",
+    "Accept-Language": "en-US,en;q=0.9",
 }
 
 response = requests.get(URL, headers=HEADER)
@@ -22,11 +22,11 @@ title = soup.find(name="span", id="productTitle").get_text().strip()
 load_dotenv()
 if price < BUY_PRICE:
     message = f"{title} is now ${price}"
-    with smtplib.SMTP(os.environ.get("SMTP_ADDRESS"), port=587) as connection:
+    with smtplib.SMTP(os.getenv("SMTP_ADDRESS"), port=587) as connection:
         connection.starttls()
-        connection.login(os.environ.get("EMAIL"), os.environ.get("PASSWORD"))
+        connection.login(os.getenv("EMAIL"), os.getenv("PASSWORD"))
         connection.sendmail(
-            from_addr=os.environ.get("EMAIL"),
-            to_addrs=os.environ.get("EMAIL"),
-            msg=f"Subject:Amazon Price Alert!\n\n{message}\n{URL}"
+            from_addr=os.getenv("EMAIL"),
+            to_addrs=os.getenv("EMAIL"),
+            msg=f"Subject:Amazon Price Alert!\n\n{message}\n{URL}",
         )
