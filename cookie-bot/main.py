d23780d5003a47bc3426e4b95281a7c37c7ad884
Jay Morales <jaymorales@pm.me>
2022-04-30 12:37:18 -0400

    Run pre-commit hooks

--- cookie-bot/main.py
+++ cookie-bot/main.py
@@ -12,7 +12,10 @@ driver = webdriver.Chrome(service=Service(os.environ.get("CHROME_DRIVER_PATH")))
 driver.get("http://orteil.dashnet.org/experiments/cookie/")
 
 cookie = driver.find_element(by=By.ID, value="cookie")
-item_ids = [item.get_attribute("id") for item in driver.find_elements(by=By.CSS_SELECTOR, value="#store div")]
+item_ids = [
+    item.get_attribute("id")
+    for item in driver.find_elements(by=By.CSS_SELECTOR, value="#store div")
+]
 
 timeout = time.time() + 5
 five_min = time.time() + 60 * 5
@@ -20,11 +23,24 @@ five_min = time.time() + 60 * 5
 while True:
     cookie.click()
     if time.time() > timeout:
-        prices = [price.text.split("-")[1].strip().replace(",", "")
-                  for price in driver.find_elements(by=By.CSS_SELECTOR, value="#store b") if price.text]
+        prices = [
+            price.text.split("-")[1].strip().replace(",", "")
+            for price in driver.find_elements(by=By.CSS_SELECTOR, value="#store b")
+            if price.text
+        ]
         cookie_upgrades = {int(prices[n]): item_ids[n] for n in range(len(prices))}
-        money = int("".join([char for char in driver.find_element(by=By.ID, value="money").text if char != ","]))
-        affordable_upgrades = {cost: item_id for cost, item_id in cookie_upgrades.items() if money > cost}
+        money = int(
+            "".join(
+                [
+                    char
+                    for char in driver.find_element(by=By.ID, value="money").text
+                    if char != ","
+                ],
+            ),
+        )
+        affordable_upgrades = {
+            cost: item_id for cost, item_id in cookie_upgrades.items() if money > cost
+        }
         highest_price_affordable_upgrade = max(affordable_upgrades)
         purchase_id = affordable_upgrades[highest_price_affordable_upgrade]
         driver.find_element(by=By.ID, value=purchase_id).click()
