e3b1a92dc846cb2db9dc172efd54b09a89be32f8
Jay Morales <jaymorales@pm.me>
2022-04-30 11:18:12 -0400

    Run pre-commit hooks

--- bootstrap-blog/main.py
+++ bootstrap-blog/main.py
@@ -1,5 +1,5 @@
+import os
 import smtplib
-from os import environ as env
 
 import requests
 from dotenv import load_dotenv
@@ -7,7 +7,7 @@ from flask import Flask, render_template, request
 
 posts = requests.get(
     "https://gist.githubusercontent.com/gellowg/389b1e4d6ff8effac71badff67e4d388/raw"
-    "/fc31e41f8e1a6b713eafb9859f3f7e335939d518/data.json"
+    "/fc31e41f8e1a6b713eafb9859f3f7e335939d518/data.json",
 ).json()
 
 load_dotenv()
@@ -44,10 +44,14 @@ def show_post(index):
 
 def send_email(name, email, phone, message):
     email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
-    with smtplib.SMTP(env.get("EMAIL_SMTP"), port=587) as connection:
+    with smtplib.SMTP(os.getenv("EMAIL_SMTP"), port=587) as connection:
         connection.starttls()
-        connection.login(env.get("EMAIL"), env.get("EMAIL_PASSWORD"))
-        connection.sendmail(from_addr=env.get("EMAIL"), to_addrs=env.get("EMAIL"), msg=email_message)
+        connection.login(os.getenv("EMAIL"), os.getenv("EMAIL_PASSWORD"))
+        connection.sendmail(
+            from_addr=os.getenv("EMAIL"),
+            to_addrs=os.getenv("EMAIL"),
+            msg=email_message,
+        )
 
 
 if __name__ == "__main__":
