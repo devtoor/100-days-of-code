2b598912d8a65e0f6487801c34a9a71dc77662b2
Jay Morales <jaymorales@pm.me>
2022-04-30 11:13:51 -0400

    Run pre-commit hooks

--- blog/main.py
+++ blog/main.py
@@ -1,20 +1,21 @@
 import requests as requests
 from flask import Flask, render_template
-
 from post import Post
 
 app = Flask(__name__)
 
 posts = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
-post_objects = [Post(post["id"], post["title"], post["subtitle"], post["body"]) for post in posts]
+post_objects = [
+    Post(post["id"], post["title"], post["subtitle"], post["body"]) for post in posts
+]
 
 
-@app.route('/')
+@app.route("/")
 def home():
     return render_template("index.html", posts=post_objects)
 
 
-@app.route('/post/<int:index>')
+@app.route("/post/<int:index>")
 def show_post(index):
     requested_post = None
     for blog_post in post_objects:
