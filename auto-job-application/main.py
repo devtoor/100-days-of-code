a47419cee7d3fb8301b0497c07c0c3c02a7297fa
Jay Morales <jaymorales@pm.me>
2022-04-30 11:09:23 -0400

    environ.get -> getenv

--- auto-job-application/main.py
+++ auto-job-application/main.py
@@ -9,7 +9,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
 load_dotenv()
-driver = webdriver.Chrome(service=Service(os.environ.get("CHROME_DRIVER_PATH")))
+driver = webdriver.Chrome(service=Service(os.getenv("CHROME_DRIVER_PATH")))
 driver.get(
     "https://www.linkedin.com/jobs/search?keywords=python%2Bdeveloper&location=Troy%2C%2BNew%2BYork%2C%2BUnited"
     "%2BStates&geoId=103000818&trk=public_jobs_jobs-search-bar_search-submit&currentJobId=2949978366&position=7"
@@ -20,9 +20,9 @@ driver.find_element(by=By.LINK_TEXT, value="Sign in").click()
 time.sleep(5)
 
 email_field = driver.find_element(by=By.ID, value="username")
-email_field.send_keys(os.environ.get("ACCOUNT_EMAIL"))
+email_field.send_keys(os.getenv("ACCOUNT_EMAIL"))
 password_field = driver.find_element(by=By.ID, value="password")
-password_field.send_keys(os.environ.get("ACCOUNT_PASSWORD"))
+password_field.send_keys(os.getenv("ACCOUNT_PASSWORD"))
 password_field.send_keys(Keys.ENTER)
 time.sleep(5)
 
@@ -44,7 +44,7 @@ for listing in all_listings:
             value="fb-single-line-text__input",
         )
         if phone.text == "":
-            phone.send_keys(os.environ.get("PHONE"))
+            phone.send_keys(os.getenv("PHONE"))
 
         submit_button = driver.find_element(by=By.CSS_SELECTOR, value="footer button")
         if submit_button.get_attribute("data-control-name") == "continue_unify":
