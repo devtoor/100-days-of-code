266df85e734c68629bd34984a54ec4a6a7d6a6a9
Jay Morales <jaymorales@pm.me>
2022-05-02 10:10:24 -0400

    Run pre-commit hooks

--- us-states-game/main.py
+++ us-states-game/main.py
@@ -18,19 +18,23 @@ writer = turtle.Turtle()
 writer.hideturtle()
 writer.penup()
 
-guessed_states = []
+guessed_states: list[str] = []
 while len(guessed_states) < 50:
     user_input = screen.textinput(
         title=f"{len(guessed_states)}/50 States Correct",
         prompt="What's another state's name?",
-    ).title()
-    if user_input == "Exit":
-        missing_states = [state for state in all_states if state not in guessed_states]
-        df = pandas.DataFrame(missing_states)
-        df.to_csv("states_to_learn.csv")
-        break
-    if user_input in all_states:
-        guessed_states.append(user_input)
-        state_data = data[data.state == user_input]
-        writer.goto(int(state_data.x), int(state_data.y))
-        writer.write(user_input)
+    )
+    if user_input is not None:
+        user_input = user_input.title()
+        if user_input == "Exit":
+            missing_states = [
+                state for state in all_states if state not in guessed_states
+            ]
+            df = pandas.DataFrame(missing_states)
+            df.to_csv("states_to_learn.csv")
+            break
+        if user_input in all_states:
+            guessed_states.append(user_input)
+            state_data = data[data.state == user_input]
+            writer.goto(int(state_data.x), int(state_data.y))
+            writer.write(user_input)
