e7d1e4f46cdc620183e2b7fca9169d07ea49f414
Jay Morales <jaymorales@pm.me>
2022-05-02 10:00:56 -0400

    Run pre-commit hooks

--- quizzler-app/data.py
+++ quizzler-app/data.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import requests
 
-parameters = {"amount": 10, "type": "boolean"}
+parameters = {"amount": "10", "type": "boolean"}
 response = requests.get(url="https://opentdb.com/api.php", params=parameters)
 response.raise_for_status()
 question_data = response.json()["results"]
